ca92fdc25da99afdfe33447aefb2cd270c5b5e4d
Jason Sams <jsams@google.com>
2013-02-05 16:07:10 -0800

    Update for API 18
    bug 7968563
    New defaults for createFromBitmap
    
    Change-Id: I224e27cd01ba5502cb833221ac731384785108eb

--- src/com/android/galaxy4/GalaxyRS.java
+++ src/com/android/galaxy4/GalaxyRS.java
@@ -106,7 +106,9 @@ public class GalaxyRS {
     }
 
     private Allocation loadTexture(int id) {
-        final Allocation allocation = Allocation.createFromBitmapResource(mRS, mRes, id);
+        final Allocation allocation = Allocation.createFromBitmapResource(mRS, mRes, id,
+                                           Allocation.MipmapControl.MIPMAP_NONE,
+                                           Allocation.USAGE_GRAPHICS_TEXTURE);
         return allocation;
     }
 
