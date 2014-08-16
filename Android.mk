9f16f030f157e3987f63b16be2c26499b84eee22
Stephen Hines <srhines@google.com>
2014-08-16 00:46:25 -0700

    Update Galaxy to use API level 14 to prepare for 64-bit compiles.
    Bug: 16031597
    Change-Id: Ie5bf8c4642a65f1ed5357c632c124de1e0f6d8be

--- Android.mk
+++ Android.mk
@@ -23,6 +23,7 @@ LOCAL_SRC_FILES := $(call all-subdir-java-files) $(call all-renderscript-files-u
 
 LOCAL_PACKAGE_NAME := Galaxy4
 LOCAL_CERTIFICATE := shared
+LOCAL_SDK_VERSION := 14
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.cfg
 
